ae7b8fa3f48f7667fe502c597a4feadf65e3c1e6
Andrew Kain <hello@andreww.io>
2022-07-18 03:20:33 +0400

    simple useReducer counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 }
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+  
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
 
   function increment() {
-    setCount(prevCount => prevCount + 1)
+    dispatch({ type: ACTIONS.INCREMENT })
   }
   
   function decrement() {
-    setCount(prevCount => prevCount - 1)
+    dispatch({ type: ACTIONS.DECREMENT })
   }
 
   return (
     <>
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </>
   );
 }
 
 export default App;
+
+
+// import { useState } from 'react';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(prevCount => prevCount + 1)
+//   }
+  
+//   function decrement() {
+//     setCount(prevCount => prevCount - 1)
+//   }
+
+//   return (
+//     <>
+//       <button onClick={decrement}>-</button>
+//       <span>{count}</span>
+//       <button onClick={increment}>+</button>
+//     </>
+//   );
+// }
+
+// export default App;
